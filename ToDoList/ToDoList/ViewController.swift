c286f15dbadcecb2cb07a5cd30645a556549494c
xiaoyipeng <helloxyp@gmail.com>
2016-02-17 20:41:06 +0800

    添加一些数据

--- ToDoList/ToDoList/ViewController.swift
+++ ToDoList/ToDoList/ViewController.swift
@@ -14,14 +14,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        todos.append(TodoModel(id: "1", image: "child-selected", title: "1,在家带孩子", date: dateFromString("2016-02-17")!))
+        todos.append(TodoModel(id: "2", image: "phone-selected", title: "2,打电话", date: dateFromString("2016-02-18")!))
+        todos.append(TodoModel(id: "3", image: "shopping-cart-selected", title: "1,购物", date: dateFromString("2016-02-19")!))
+        todos.append(TodoModel(id: "4", image: "travel-selected", title: "1,旅游", date: dateFromString("2016-02-20")!))
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func dateFromString(string: String) -> NSDate? {
+        let df = NSDateFormatter()
+        df.dateFormat = "yyyy-MM-dd"
+        let dt = df.dateFromString(string)
+        return dt
     }
-
-
 }
 
